9d86e303f7dd6c3ed971b3da999fa2b56543b35e
prashantkrmathur <prashantnawa123@gmail.com>
2023-07-24 10:12:39 +0530

    Final commit

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Dashboard = (props) => {
   console.log("props", props.location.state)
   return (
-    <div>
+    <div style={{ textAlign: "-webkit-center", padding: "2em", backgroundColor: "azure" }}>
       <h3 style={{ textAlign: "center", margin: "5px" }}>{props.location.state.message}</h3>
-      <h4> Full Name : {`${props.location.state.user.firstName} ${props.location.state.user.lastName}`}</h4>
-      <h4> Email : {props.location.state.user.email }</h4>
-      <h4> Username : {props.location.state.user.userName}</h4>
-      <h4> Token : {props.location.state.token }</h4>
+      <h6> Full Name : {`${props.location.state.user.firstName} ${props.location.state.user.lastName}`}</h6>
+      <h6> Email : {props.location.state.user.email }</h6>
+      <h6> Username : {props.location.state.user.userName}</h6>
+      <h6> Token : {props.location.state.token }</h6>
     </div>
   )
 }
